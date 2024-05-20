c4444bc62d7ea5e6a316657ab24feaeb78849660
shasanka296 <sla296@uky.edu>
2024-05-20 14:40:16 -0400

    code working for titration

--- Fast/titrationPlotter.py
+++ Fast/titrationPlotter.py
@@ -9,6 +9,6 @@ def TitrationPlotter( Average_simulation_density_dic, key):
     plt.xlabel("System")
     plt.ylabel("Simulation density")
     plt.title(f"Titration Result{key}")
-    #plt.savefig(f"/mnt/gpfs2_4m/scratch/sla296/test_run/output_of_runs/Output{key}/TitrationGraph_{key}.pdf")
-    plt.show()
+    plt.savefig(f"/mnt/gpfs2_4m/scratch/sla296/test_run/output_of_runs/Output{key}/TitrationGraph_{key}.pdf")
+
 
