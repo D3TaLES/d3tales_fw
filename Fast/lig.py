2939b6f8a9eb7c43d1be7fd298b2601fdfe2c316
shasanka296 <sla296@uky.edu>
2024-05-21 11:45:03 -0400

    not wroking

--- Fast/lig.py
+++ Fast/lig.py
@@ -20,20 +20,23 @@ class lig:
 
         cmd = ["singularity", "exec", singularity_container, "bash", "-c",
                f'{conda_activate} && {export_bossdir} && {ligpargen_cmd}']
-        subprocess.Popen(cmd).wait()
+        try:
+            subprocess.Popen(cmd).wait()
 
-        while os.path.isfile(f'{self.dir}/{self.mol}/{self.mol}-debug.pdb') == False:
-            print("still making pdb")
-            time.sleep(1)
-        pdb_location = Chem.MolFromPDBFile(f'{self.dir}/{self.mol}/{self.mol}-debug.pdb')
-        pdb_location = Chem.AddHs(pdb_location)
-        AllChem.EmbedMolecule(pdb_location)
-        Chem.MolToMolFile(pdb_location, f'{self.dir}/{self.mol}/{self.mol}.mol')
-        comand2 = f'obabel -imol {self.dir}/{self.mol}/{self.mol}.mol -ogjf -O {self.dir}/{self.mol}script/{self.mol}.gjf --gen3D'
-        comand3 = f'obabel -igro {self.dir}/{self.mol}/{self.mol}.gmx.gro -opdb -O {self.dir}/{self.mol}/{self.mol}.pdb '
-        print(comand3)
-        subprocess.run([comand2], shell=True)
-        subprocess.run([comand3], shell=True)
+            while os.path.isfile(f'{self.dir}/{self.mol}/{self.mol}-debug.pdb') == False:
+                print("still making pdb")
+                time.sleep(1)
+            pdb_location = Chem.MolFromPDBFile(f'{self.dir}/{self.mol}/{self.mol}-debug.pdb')
+            pdb_location = Chem.AddHs(pdb_location)
+            AllChem.EmbedMolecule(pdb_location)
+            Chem.MolToMolFile(pdb_location, f'{self.dir}/{self.mol}/{self.mol}.mol')
+            comand2 = f'obabel -imol {self.dir}/{self.mol}/{self.mol}.mol -ogjf -O {self.dir}/{self.mol}script/{self.mol}.gjf --gen3D'
+            comand3 = f'obabel -igro {self.dir}/{self.mol}/{self.mol}.gmx.gro -opdb -O {self.dir}/{self.mol}/{self.mol}.pdb '
+            print(comand3)
+            subprocess.run([comand2], shell=True)
+            subprocess.run([comand3], shell=True)
+        except:
+
 
     def PDBMAKER(self, name, smiles, type):
         subprocess.run([
