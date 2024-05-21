1c8e3cf0c1d255cb76d9abdd6d57935e09c0bb13
shasanka296 <sla296@uky.edu>
2024-05-21 16:27:06 -0400

    for pc

--- workflows/wf_writer.py
+++ workflows/wf_writer.py
@@ -89,10 +89,6 @@ def d3tales_md_wf(param_file=None, **kwargs):
             name_dic[f"names{i + 1}"] = kwargs.get(f"WF_name{i + 1}")
         if kwargs.get("own") == False:
 
-
-
-
-
             for smiles, (index,names) in zip(kwargs.get("smiles_list"), enumerate(kwargs.get("name_list"))):
                 d = Optimization(paramset=paramset.opt_groundState,
                                                              species="groundState",
@@ -100,12 +96,13 @@ def d3tales_md_wf(param_file=None, **kwargs):
                 dft_fw.append(d)
                 folder.append(fo(name=names,parents=d,**kwargs))
 
-            for typ, name, smiles in zip(kwargs.get("type_list"), kwargs.get("name_list"), kwargs.get("smiles_list")
-                                         ):
-                ligpargen_fws.append(Ligpargen_FW(name=name, smiles=smiles, con=0, Type=typ, di=kwargs.get("dir"),parents=folder, **kwargs))
-                for i in range(number_of_systems):
-                    if str((i + 1)) in name:
-                        name_dic[f"names{i + 1}"] = name_dic[f"names{i + 1}"] + f"_{name}"
+
+        for typ, name, smiles in zip(kwargs.get("type_list"), kwargs.get("name_list"), kwargs.get("smiles_list")
+                                     ):
+            ligpargen_fws.append(Ligpargen_FW(name=name, smiles=smiles, con=0, Type=typ, di=kwargs.get("dir"),parents=folder, **kwargs))
+            for i in range(number_of_systems):
+                if str((i + 1)) in name:
+                    name_dic[f"names{i + 1}"] = name_dic[f"names{i + 1}"] + f"_{name}"
 
 
 
@@ -223,128 +220,128 @@ def d3tales_md_wf(param_file=None, **kwargs):
 
 
 
-            for typ, name, smiles in zip(kwargs.get("type_list"), kwargs.get("name_list"), kwargs.get("smiles_list")
-                                         ):
-                ligpargen_fws.append(
-                    Ligpargen_FW(name=f"{name}", smiles=smiles, con=0, Type=typ, di=kwargs.get("dir"), parents=folder,
-                                 **kwargs))
-                for i in range(outer_system):
-                    if str((i + 1)) in name:
-                        name_dic[f"names{i + 1}"] = name_dic[f"names{i + 1}"] + f"_{name}"
-            print(f"the name dic is {name_dic}")
-            print(f'type list:{kwargs.get("type_list")} name_lis:{kwargs.get("name_list")} smiles_list:{ kwargs.get("smiles_list")} ')
+        for typ, name, smiles in zip(kwargs.get("type_list"), kwargs.get("name_list"), kwargs.get("smiles_list")
+                                     ):
+            ligpargen_fws.append(
+                Ligpargen_FW(name=f"{name}", smiles=smiles, con=0, Type=typ, di=kwargs.get("dir"), parents=folder,
+                             **kwargs))
+            for i in range(outer_system):
+                if str((i + 1)) in name:
+                    name_dic[f"names{i + 1}"] = name_dic[f"names{i + 1}"] + f"_{name}"
+        print(f"the name dic is {name_dic}")
+        print(f'type list:{kwargs.get("type_list")} name_lis:{kwargs.get("name_list")} smiles_list:{ kwargs.get("smiles_list")} ')
 
 
-        for i in range(outer_system):
-            Index_key_to_pull= i* number_of_titrations
-            fw_pack_key = f"fw_pack{i + 1}"
-            fire_workdic[fw_pack_key] = Pack_FW (
-                name=name_dic[f"names{Index_key_to_pull + 1}"] + 'pack',
-                parents=ligpargen_fws,
-                solute_name=kwargs.get(f"solute_name{i + 1}"),
-                solvent_name=kwargs.get(f"solvent_name{i + 1}"),
-                solute_smiles=kwargs.get(f"solute_smiles{i + 1}"),
-                solvent_smiles=kwargs.get(f"solvent_smiles{i + 1}"),
-                x=kwargs.get(f"x{i + 1}"),
-                y=kwargs.get(f"y{i + 1}"),
-                z=kwargs.get(f"z{i + 1}"),
-                di=kwargs.get("dir"),
-                conmatrix=kwargs.get(f"conmatrix{i + 1}"),
-                den=kwargs.get(f"den{i + 1}"), key=key_mat[Index_key_to_pull]
-            )
-            matrix_of_titration.append( Titrate(
-                name=name_dic[f"names{Index_key_to_pull + 1}"] + 'Titrate',
-                parents=fire_workdic[fw_pack_key],
-                solute_name=kwargs.get(f"solute_name{i + 1}"),
-                solvent_name=kwargs.get(f"solvent_name{i + 1}"),
-                solute_smiles=kwargs.get(f"solute_smiles{i + 1}"),
-                solvent_smiles=kwargs.get(f"solvent_smiles{i + 1}"),
-                x=kwargs.get(f"x{i + 1}"),
-                y=kwargs.get(f"y{i + 1}"),
-                z=kwargs.get(f"z{i + 1}"),
-                di=kwargs.get("dir"),
-                conmatrix=kwargs.get(f"conmatrix{i + 1}"),
-                den=kwargs.get(f"den{i + 1}"), key=key_mat[Index_key_to_pull], titration_list=kwargs.get("titartion_list")
-            )   )
-            print(f'in the workflow this is waht is ebing passed for titration list{kwargs.get("titartion_list")}')
-        densities=[kwargs.get(f"den{i + 1}") for i in range(outer_system) for j in range(number_of_titrations)]
-        print(f"waht is passed in as  density {densities[0]}")
-        molarmasses=[kwargs.get(f"MM{i + 1}") for i in range(outer_system) for j in range(number_of_titrations)]
+    for i in range(outer_system):
+        Index_key_to_pull= i* number_of_titrations
+        fw_pack_key = f"fw_pack{i + 1}"
+        fire_workdic[fw_pack_key] = Pack_FW (
+            name=name_dic[f"names{Index_key_to_pull + 1}"] + 'pack',
+            parents=ligpargen_fws,
+            solute_name=kwargs.get(f"solute_name{i + 1}"),
+            solvent_name=kwargs.get(f"solvent_name{i + 1}"),
+            solute_smiles=kwargs.get(f"solute_smiles{i + 1}"),
+            solvent_smiles=kwargs.get(f"solvent_smiles{i + 1}"),
+            x=kwargs.get(f"x{i + 1}"),
+            y=kwargs.get(f"y{i + 1}"),
+            z=kwargs.get(f"z{i + 1}"),
+            di=kwargs.get("dir"),
+            conmatrix=kwargs.get(f"conmatrix{i + 1}"),
+            den=kwargs.get(f"den{i + 1}"), key=key_mat[Index_key_to_pull]
+        )
+        matrix_of_titration.append( Titrate(
+            name=name_dic[f"names{Index_key_to_pull + 1}"] + 'Titrate',
+            parents=fire_workdic[fw_pack_key],
+            solute_name=kwargs.get(f"solute_name{i + 1}"),
+            solvent_name=kwargs.get(f"solvent_name{i + 1}"),
+            solute_smiles=kwargs.get(f"solute_smiles{i + 1}"),
+            solvent_smiles=kwargs.get(f"solvent_smiles{i + 1}"),
+            x=kwargs.get(f"x{i + 1}"),
+            y=kwargs.get(f"y{i + 1}"),
+            z=kwargs.get(f"z{i + 1}"),
+            di=kwargs.get("dir"),
+            conmatrix=kwargs.get(f"conmatrix{i + 1}"),
+            den=kwargs.get(f"den{i + 1}"), key=key_mat[Index_key_to_pull], titration_list=kwargs.get("titartion_list")
+        )   )
+        print(f'in the workflow this is waht is ebing passed for titration list{kwargs.get("titartion_list")}')
+    densities=[kwargs.get(f"den{i + 1}") for i in range(outer_system) for j in range(number_of_titrations)]
+    print(f"waht is passed in as  density {densities[0]}")
+    molarmasses=[kwargs.get(f"MM{i + 1}") for i in range(outer_system) for j in range(number_of_titrations)]
 
-        for i in range(number_of_systems):
+    for i in range(number_of_systems):
 
 
-            fw_em_key = f"fw_em{i + 1}"
-            fire_workdic[fw_em_key] = EM_FW(
-                name=name_dic[f"names{i + 1}"] + "EM",
-                parents= matrix_of_titration, key=key_mat[i],
-                **kwargs
-            )
+        fw_em_key = f"fw_em{i + 1}"
+        fire_workdic[fw_em_key] = EM_FW(
+            name=name_dic[f"names{i + 1}"] + "EM",
+            parents= matrix_of_titration, key=key_mat[i],
+            **kwargs
+        )
 
-            fw_nvt_key = f"fw_nvt{i + 1}"
-            fire_workdic[fw_nvt_key] = NVT_FW(
-                name=name_dic[f"names{i + 1}"] + "NVT",
-                parents=fire_workdic[fw_em_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_nvt_key = f"fw_nvt{i + 1}"
+        fire_workdic[fw_nvt_key] = NVT_FW(
+            name=name_dic[f"names{i + 1}"] + "NVT",
+            parents=fire_workdic[fw_em_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_npt_key = f"fw_npt{i + 1}"
-            fire_workdic[fw_npt_key] = NPT_FW(
-                name=name_dic[f"names{i + 1}"] + "NPT",
-                parents=fire_workdic[fw_nvt_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_npt_key = f"fw_npt{i + 1}"
+        fire_workdic[fw_npt_key] = NPT_FW(
+            name=name_dic[f"names{i + 1}"] + "NPT",
+            parents=fire_workdic[fw_nvt_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_den_key = f"fw_den{i + 1}"
-            fire_workdic[fw_den_key] = Density_FW(
-                name=name_dic[f"names{i + 1}"] + "DEN",
-                parents=fire_workdic[fw_npt_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_den_key = f"fw_den{i + 1}"
+        fire_workdic[fw_den_key] = Density_FW(
+            name=name_dic[f"names{i + 1}"] + "DEN",
+            parents=fire_workdic[fw_npt_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_prod_key = f"fw_prod{i + 1}"
-            fire_workdic[fw_prod_key] = Check_FW(
-                name=name_dic[f"names{i + 1}"] + "prod",
-                parents=fire_workdic[fw_den_key], key=key_mat[i], den=densities[i],
-                mm=molarmasses[i],
-                **kwargs
-            )
+        fw_prod_key = f"fw_prod{i + 1}"
+        fire_workdic[fw_prod_key] = Check_FW(
+            name=name_dic[f"names{i + 1}"] + "prod",
+            parents=fire_workdic[fw_den_key], key=key_mat[i], den=densities[i],
+            mm=molarmasses[i],
+            **kwargs
+        )
 
-            fw_trj_key = f"fw_trj{i + 1}"
-            fire_workdic[fw_trj_key] = TR_FW(
-                name=name_dic[f"names{i + 1}"] + "TRJ",
-                parents=fire_workdic[fw_prod_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_trj_key = f"fw_trj{i + 1}"
+        fire_workdic[fw_trj_key] = TR_FW(
+            name=name_dic[f"names{i + 1}"] + "TRJ",
+            parents=fire_workdic[fw_prod_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_index_key = f"fw_index{i + 1}"
-            fire_workdic[fw_index_key] = Index_FW(
-                name=name_dic[f"names{i + 1}"] + "Index",
-                parents=fire_workdic[fw_trj_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_index_key = f"fw_index{i + 1}"
+        fire_workdic[fw_index_key] = Index_FW(
+            name=name_dic[f"names{i + 1}"] + "Index",
+            parents=fire_workdic[fw_trj_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_resi_key = f"fw_resi{i + 1}"
-            fire_workdic[fw_resi_key] = RES_FW(
-                name=name_dic[f"names{i + 1}"] + "RESI",
-                parents=fire_workdic[fw_index_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_resi_key = f"fw_resi{i + 1}"
+        fire_workdic[fw_resi_key] = RES_FW(
+            name=name_dic[f"names{i + 1}"] + "RESI",
+            parents=fire_workdic[fw_index_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_rdf_key = f"fw_rdf{i + 1}"
-            fire_workdic[fw_rdf_key] = RDF_FW(
-                name=name_dic[f"names{i + 1}"] + "RDF",
-                parents=fire_workdic[fw_resi_key], key=key_mat[i],
-                **kwargs
-            )
+        fw_rdf_key = f"fw_rdf{i + 1}"
+        fire_workdic[fw_rdf_key] = RDF_FW(
+            name=name_dic[f"names{i + 1}"] + "RDF",
+            parents=fire_workdic[fw_resi_key], key=key_mat[i],
+            **kwargs
+        )
 
-            fw_cord_key = f"fw_cord{i + 1}"
-            fire_workdic[fw_cord_key] = CORD_FW(
-                name=name_dic[f"names{i + 1}"] + "CORD",
-                parents=fire_workdic[fw_rdf_key], key=key_mat[i],
-                **kwargs
-            )
-            print(f"{i+1} iteration")
+        fw_cord_key = f"fw_cord{i + 1}"
+        fire_workdic[fw_cord_key] = CORD_FW(
+            name=name_dic[f"names{i + 1}"] + "CORD",
+            parents=fire_workdic[fw_rdf_key], key=key_mat[i],
+            **kwargs
+        )
+        print(f"{i+1} iteration")
     print("made regualr")
     regula = []
     plotter=[]
