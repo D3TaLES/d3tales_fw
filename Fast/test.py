98fd294c2948f47b43b83b849f2d95505961d990
shasanka296 <sla296@uky.edu>
2024-05-21 11:39:09 -0400

    testing out pc, code is not opertaional

--- Fast/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Command to run the shell script and gmx_mpi together
-command = 'source /project/cmri235_uksr/shasanka_conda_boss/sla296/singularity/Fast/ASMD1/Input/gromacs_gpu.sh && gmx_mpi'
-
-# Run the command in a subprocess with shell=True
-subprocess.run(command, shell=True)
-
